36d9f419c1cb6b4af3948b6db8a0d14d92b24929
lowhornj <lowhornj@merrimack.edu>
2022-05-14 22:40:54 -0400

    updated week 2

--- Week_2/Examples/Ex3_data_frames.R
+++ Week_2/Examples/Ex3_data_frames.R
@@ -42,5 +42,63 @@ summary(sales_csv)
 # Get the structure
 str(sales_csv)
 
+########################### Characters/Strings #############################
 
-sales_csv$Order.Date <- as.Date(sales_csv$Order.Date,format='%m/%d/%Y')
+# Splitting strings to create two new columns
+## String split fixed will split the product id column into three columns in a matrix by the '-'
+temp_char <- stringr::str_split_fixed(string=sales_csv$Product.ID,pattern='-',n=3)
+# To recreate our Product ID we just paste the individual vectors together with the '-' seperator
+sales_csv$Product <- paste(temp_char[,1], temp_char[,2],sep='-')
+# Our product number is now just the third column of the matrix
+sales_csv$Product.Number <- temp_char[,3]
+
+cat(
+  sales_csv$Product.ID[1]
+  ,sales_csv$Product[1]
+  ,sales_csv$Product.Number[1]
+  ,sep="\n\r"
+)
+
+################################### Factors ##################################
+# Check whether Region is a factor & find the unique values. 
+is.factor(sales_csv$Region)
+unique(sales_csv$Region)
+
+# Convert the Region column to a factor and extract the levels
+sales_csv$Region <- factor(sales_csv$Region)
+is.factor(sales_csv$Region)
+levels(sales_csv$Region)
+
+# Create a basic barplot
+barplot(table(sales_csv$Region))
+
+# What if we wanted to change the order of the categories in the barplot?
+# Use factors and reorder the levels
+sales_csv$Region <- factor(sales_csv$Region
+                           ,levels=c('West','East','Central','South')
+                           )
+levels(sales_csv$Region)
+
+barplot(table(sales_csv$Region))
+
+################################### Dates ##################################
+# Convert the Order.Date string to Date format
+
+# Get the structure of the date column
+str(sales_csv$Order.Date)
+
+# Check to see if Order.Date is a date
+inherits(sales_csv$Order.Date
+         , c("Date")
+         )
+
+# Using the table in our notes, convert the character to a date obeject. 
+sales_csv$Order.Date <- as.Date(sales_csv$Order.Date
+                                ,format='%m/%d/%Y'
+                                )
+
+# Check to see if our conversion worked
+inherits(sales_csv$Order.Date
+         , c("Date")
+         )
+str(sales_csv$Order.Date)
