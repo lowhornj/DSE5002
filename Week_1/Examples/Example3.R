9163e25c22f28ea9af122f9f41c37a9e980a58bb
lowhornj <104390957+lowhornj@users.noreply.github.com>
2023-10-30 17:49:01 -0500

    adjusted week 1

--- Week_1/Examples/Example3.R
+++ Week_1/Examples/Example3.R
@@ -2,17 +2,23 @@ x <- 10
 y <- 2
 
 test_vector <- c(2,5,6)
-vec_2 <- c(TRUE,FALSE,FALSE)
+vec_2 <- c(FALSE,TRUE,FALSE)
 
 x %in% test_vector | y %in% test_vector
 
 test_vector[x == test_vector]
 test_vector[y == test_vector]
 
-test_vector[]
+test_vector[vec_2]
+
 
 z = 6
 
-test_vector[   (x==test_vector)|(y==test_vector)|(z==test_vector)   ]
+logic_exp <- (x==test_vector)|(y==test_vector)|(z==test_vector)
+
+length(logic_exp) == length(test_vector)
+
+test_vector[  logic_exp   ]
+
 
 test_vector[x== test_vector] | test_vector[y== test_vector] 
